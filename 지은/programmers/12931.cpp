57595e0f769bb1028cb88f60693696924e699030
최지은 <cje172@naver.com>
2023-05-22 21:19:13 +0900

    [programmers] 자릿수 더하기
    :Explanation:
    자릿수를 더하여 결과 출력
    
    :Language:
    C++
    
    :Algorithm:
    수학, 사칙연산
    
    :Link:
    https://school.programmers.co.kr/learn/courses/30/lessons/12931

---
+++ 지은/programmers/12931.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+	int answer = 0;
+
+	while (n > 0) {
+		answer += n % 10;
+		n /= 10;
+	}
+
+	return answer;
+}
