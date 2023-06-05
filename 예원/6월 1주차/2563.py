dcca2fc81fab3df0a71ef7a425652f210f2f465d
kiiiiv <lizzy2@naver.com>
2023-06-05 16:50:06 +0900

    Update 2563.py

--- 예원/6월 1주차/2563.py
+++ 예원/6월 1주차/2563.py
@@ -1,2 +1,13 @@
-for i in range(3):
-    list
+N = int(input())
+paper = [[0 for _ in range(101)] for _ in range(101)]
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            paper[i][j] = 1
+
+answer = 0
+for row in paper:
+    answer += row.count(1)
+print(answer)
