35990660bf1ce849f3bb3d5a65f28caf86c94668
최지은 <cje172@naver.com>
2023-07-17 21:19:58 +0900

    [programmers] 추억 점수

---
+++ 지은/programmers/176963.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+	vector<int> answer;
+
+	map<string, int> m;
+	for (int i = 0; i < name.size(); i++)
+		m[name[i]] = yearning[i];
+    
+	for (int i = 0; i < photo.size(); i++) {
+		int sum = 0;
+		for (int j = 0; j < photo[i].size(); j++) 
+			sum += m[photo[i][j]];
+		answer.push_back(sum);
+	}
+	return answer;
+}
