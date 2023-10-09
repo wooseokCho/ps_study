832c9fb59096f85259039f6f412216ffae152700
IJS1016 <imjungsunee@gmail.com>
2023-10-10 00:10:22 +0900

    [23년10월1주차] Programmer PS

---
+++ 정선/programmers/중복된숫자개수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int n) {
+    int answer = 0;
+    
+    for (int i=0; i<array.size(); i++) {
+        if (array[i] == n) {
+            answer += 1;
+        }
+    }
+    return answer;
+}
