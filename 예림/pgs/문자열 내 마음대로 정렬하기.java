3273b0392dec247401e78e1707ec8dee8c5cc193
EomYerim <djadpfa1002@naver.com>
2023-11-14 11:02:43 +0900

    [Lv.1] 문자열 내 마음대로 정렬하기
    :Explanation:
    문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하기
    
    :Language:
    JAVA
    
    :Algorithm:
    정렬
    
    :Link:
    https://school.programmers.co.kr/learn/courses/30/lessons/12915

---
+++ 예림/pgs/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+  public String[] solution(String[] strings, int n) {
+      Arrays.sort(strings, new Comparator<String>(){
+          @Override
+          public int compare(String s1, String s2){
+              if(s1.charAt(n) > s2.charAt(n)) return 1;
+              else if(s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
+              else if(s1.charAt(n) < s2.charAt(n)) return -1;
+              else return 0;
+          }
+      });
+      return strings;
+  }
+}
