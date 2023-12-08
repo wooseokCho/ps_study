176aa8f82e0cfa4ac130b1ff731d4f778a7f6e86
최지은 <cje172@naver.com>
2023-12-09 02:08:13 +0900

    [programmers] 점프와 순간 이동

---
+++ 지은/programmers/12980.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+
+int solution(int n) {
+    int ans = 0;
+    
+    while (n > 0) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n--;
+            ans++;
+        }
+    }    
+
+    return ans;
+}
