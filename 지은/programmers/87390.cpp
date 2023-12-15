bd99bcd4b65ff80d70096c236b0b3978ad5a87f4
최지은 <cje172@naver.com>
2023-12-15 19:20:38 +0900

    [programmers] n^2 배열 자르기

---
+++ 지은/programmers/87390.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    vector<long long> v(n*n);
+    
+    for (long long i = left; i <= right; i++) {
+        int col = i % n;
+        int row = i / n;
+        
+        answer.push_back(max(col, row) + 1);
+    }
+    
+    return answer;
+}
