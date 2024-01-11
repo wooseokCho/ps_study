9d3e61e522ee883f506cfe5479fa6047562eda68
IJS1016 <imjungsunee@gmail.com>
2024-01-12 00:02:09 +0900

    [2401_2W]python PS - 삼성기출

--- 정선/python/baekjoon/14499_주사위굴리기.py
+++ 정선/python/baekjoon/14499_주사위굴리기.py
@@ -1,3 +1,4 @@
+# 개인 풀이 과정 #########
 # 지도
 # 동쪽 남쪽
 # 주사위 위치
@@ -8,86 +9,83 @@
 # 0이 아닌경우 바닥 숫자가 주사위에 복사
 # 맨 윗면에 써져있는 수를 출력
 
-# 주사위를 어떻게 나타내지?
-# 리스트랑 인덱스로 나타내야겠다
+# 주사위 : 리스트랑 인덱스
+# TODO: 다른 풀이 찾아보기
+###########################
 
-# call by ref, by value 때문인가? 값이 이상한게?
-# 추가로 찾아보고,
+
 # 아래 코드도 깔끔하게 정리하기
 def copy_dice_n_map() :
     # 주사위 카피
     if map_info[y][x] == 0 :
-        map_info[y][x] = h[up_to_down(h_idx)]
+        map_info[y][x] = h[up2down(h_idx)]
     else :
-        h[up_to_down(h_idx)] = map_info[y][x]
+        h[up2down(h_idx)] = map_info[y][x]
+        w[up2down(w_idx)] = map_info[y][x]
         map_info[y][x] = 0
 
-def up_to_down(idx) :
+def up2down(idx) :
     return (idx + 2) % 4
 
-h_idx, w_idx = 0, 0
-
+# parameter init
+map_info = []
 h = [0, 0, 0, 0]
 w = [0, 0, 0, 0]
+h_idx, w_idx = 0, 0
 
-mw, mh, x, y, _ = map(int, input().split())
-map_info = []
-
-for _ in range(mw) :
+# get inputs
+mh, mw, x, y, _ = map(int, input().split())
+for _ in range(mh) :
     map_info.append(list(map(int, input().split(" "))))
-
 k_list = list(map(int, input().split()))
 
+# for dbg
+# direct=['우', '좌', '상', '하']
 
 for k in k_list :
+    # for dbg
+    # print(f"({x}, {y})")
+    # print(f"\n\n명령 {direct[k-1]} {k}")
+
     copy_dice_n_map()
-    print(f"\n\n명령 {k}")
-    # 1234
-    # 동서남북
-    # 우좌하상
-    # 위치마다 변경하도록
-    if k == 3 : # 위
-        if y <= 0 :
-            continue
-        y -= 1
-        h_idx = (h_idx + 1) % 4
-        w[w_idx] = h[h_idx]
-        w[(w_idx+2)%2] = h[(h_idx+2)%2]
 
-    elif k == 4 : # 하
-        if y >= mh :
+    if k == 3 or k == 4: # 상, 하
+        if (k == 3 and y <= 0) or (k == 4 and y >= mh-1) :
             continue
-        y += 1
-        h_idx = (h_idx - 1) % 4
-        w[w_idx] = h[h_idx]
-        w[(w_idx+2)%2] = h[(h_idx+2)%2]
-    
-    elif k == 1 : # 오
-        if x >= mw :
-            continue
-        x += 1
-        w_idx = (w_idx - 1) % 4
-        h[h_idx] = w[w_idx]
-        h[(h_idx+2)%2] = w[(w_idx+2)%2]
-
-    elif k == 2 : # 왼
-        if x <= 0 :
-            continue
-        x -= 1
-        w_idx = (w_idx + 1) % 4
-        h[h_idx] = w[w_idx]
-        h[(h_idx+2)%2] = w[(w_idx+2)%2]
-    
-    for i, mi in enumerate(map_info) :
-        if i == y :
-            print("!", end="")
+        if k == 3 :
+            tmp = -1
         else :
-            print(" ", end="")
-        print(" ".join(map(str, mi)))
-    
-    print(f"x, y : {x}, {y}")
-    print(h, h_idx)
-    print(w, w_idx)
+            tmp = 1
+        y += tmp
+        h_idx = (h_idx + tmp) % 4
+        w[w_idx] = h[h_idx]
+        w[(w_idx+2)%4] = h[(h_idx+2)%4]
 
+    elif k == 1 or k == 2: # 우, 좌
+        if (k == 1 and x >= mw-1) or (k == 2 and x <= 0) :
+            continue
+        if k == 1 :
+            tmp = 1
+        else :
+            tmp = -1
+        x += tmp
+        w_idx = (w_idx - tmp) % 4
+        h[h_idx] = w[w_idx]
+        h[(h_idx+2)%4] = w[(w_idx+2)%4]
+
+    # for dbg
+    # tmp_str = " "+"  "*x+"!"
+    # print(tmp_str)
+    # for i, mi in enumerate(map_info) :
+    #     if i == y :
+    #         print("!", end="")
+    #     else :
+    #         print(" ", end="")
+    #     print(" ".join(map(str, mi)))    
+    # print(h, h_idx)
+    # print(w, w_idx)
+
+    print(f"{h[h_idx]}")
 
         
+
