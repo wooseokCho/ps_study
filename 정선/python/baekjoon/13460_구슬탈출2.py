8b973d3af79a29429f2fe65972f71b803e089814
IJS1016 <imjungsunee@gmail.com>
2024-01-15 23:55:58 +0900

    Solving boj 13460

--- 정선/python/baekjoon/13460_구슬탈출2.py
+++ 정선/python/baekjoon/13460_구슬탈출2.py
@@ -5,33 +5,113 @@
 # #.#.#
 # #RO.#
 # #####
+import copy
+    
+def print_mmap(mmap) :
+    for m in mmap :
+        print(m)
+
+def dbg_mmap(rx, ry, bx, by, visited) :
+    visited = copy.deepcopy(visited)
+    visited[ry][rx] = 'R'
+    visited[by][bx] = 'B'
+    print_mmap(visited)
+
 
 N, M = map(int, input().split(" "))
-map = []
+mmap = []
+visited = []
 r_loc = []
 b_loc = []
 h_loc = []
 
 for w in range(N) :
-    line = input().split("")
-    if len(r_loc) == 0 and line.index('R')  :
-        r_loc = [w, line.index('R')]
-    if len(b_loc) == 0 and line.index('B')  :
-        b_loc = [w, line.index('B')]
-    if len(b_loc) == 0 and line.index('B')  :
-        b_loc = [w, line.index('B')]
-    
-    map.append(line)
+    line = input()
+    if line.find('R') != -1 :
+        r_loc = [w, line.find('R')]
+
+    if line.find('B') != -1 :
+        b_loc = [w, line.find('B')]
+
+    mmap.append(list(line))
+    visited.append(list(line.replace('R','.').replace('B','.')))
+
+print(f"R {r_loc}")
+print(f"B {b_loc}")
 
 # BFS
 # 상하좌우 순서대로 모두 수행하며 결과 저장하면서 돌기
-# 그럼 구슬 위치도 따로 저장해야 되는가?
+# 결과들을 저장하는게 에반데... map도 저장해야되니까 굳이 해야되나? 구슬 위치만 저장하면 map은 바뀌지 않잖아
+# 1번째 상하좌우 4개, 2번째 8, 3번째 16, .... 2^11 = 2048 경우의 수를 저장?하려면 하지...
+# 위로 갔으면 위로 가는 조건은 담에 필요 없지, 가만히 있는 위치를 제외한다면 괜찮으려나?
+# 상하좌우
+# 상 : 하좌우
+# 하 : 상좌우
+# 이동 방향을 R, B이 이동할 수 있는 거리로 축소한다면?
+# 시작할 때 아니면 3방향이 비어있는 경우는 있을 수 있지
+# 왔던 방향을 되돌아가는 것 제외, 같은 방향 제외하면 2방향이지
+# 그리고 이동할 수 있는 방향 추가하면 괜찮으려나?
+# 최악의 수는 2048*2만큼 저장하는거임
+# 그전에 끝나면 바로 gg
+# 만약 성공하면 그냥 끝내면 되고 실패하면
+# 그 횟수만큼은 다 돌아야지
+
 # 상일 경우 [-1, 0], 하 [1, 0], 좌  [0, -1], 우 [0, +1]
 # R,B 중 앞에 index 확인해서 가까운 것부터 벽에 부딪히기 전까지 이동하기
+# 0 : 상, 1 : 하, 2 : 좌, 3 : 우
+direct = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+dbg_direct = ['상', '하', '좌', '우']
+# visited = [visited * 4] # visited X로 표시 아! R, B이 같이 있으니 애매하네...visited 뺴고 생각
+
+tmp_queue = []
+queue = [[-1, r_loc, b_loc]] # [direction, r_loc, b_loc]
+cnt = 0
+
 while True :
-    ry, rx = r_loc
-    if map[ry-1, rx] == '#' :
-        break
-    if map[ry-1, rx] == 'O' :
-        break
-    
+    for q in queue :
+        before_direct, r_loc, b_loc = q
+        for d_idx in range(4) :
+            # 왔던 방향 되돌아가거나, 이전 방향과 같은 것 제외
+            if before_direct == d_idx or (before_direct + 2) % 4 == d_idx:
+                continue
+
+ 
+            ry, rx = r_loc
+            by, bx = b_loc
+
+            print("\n>> BEFORE")
+            dbg_mmap(rx, ry, bx, by, visited) 
+            print(f"!! {dbg_direct[d_idx]}")
+
+            my, mx = direct[d_idx]
+            m_cnt = 1 
+
+            rstop = False
+            bstop = False
+
+            # rstop, bstop 등 벽에 부딪혔을 때, 홀에 빠졌을 때 나눠서 해줘야되는뎀
+            # n번째에서 모두 다 구멍에 빠지면 실패로 간주해야됨
+            # 어렵구만 골드 1일만 해
+            # 상하좌우 돌면서 벽, 구멍까리 순서대로 tmp_queue에 슉슈슉 넣어주기, queue처럼
+            while not (rstop and bstop) :
+                mcy = my * m_cnt
+                mcx = mx * m_cnt
+                # print("R B")
+                # print(rstop, ry + mcy, rx + mcx)
+                # print(bstop, by + mcy, bx + mcx)
+                if not rstop :
+                    if mmap[ry + mcy][rx + mcx] == "#" :
+                        rstop = True
+                        ry, rx = ry + mcy - my, rx + mcx - mx
+                if not bstop :
+                    if mmap[by + mcy][bx + mcx] == "#" :
+                        bstop = True
+                        by, bx = by + mcy - my, bx + mcx - mx
+                m_cnt += 1
+
+            tmp_queue.append([d_idx, [ry, rx], [by, bx]])
+            dbg_mmap(rx, ry, bx, by, visited) 
+            input()
+    queue = tmp_queue
+    tmp_queue = []
+    cnt += 1
