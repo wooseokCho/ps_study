06b41d6bb4915903d1a0b88caea143db1343fc2a
최지은 <cje172@naver.com>
2024-03-11 18:30:20 +0900

    Week 45

---
+++ 지은/programmers/42747.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    
+    sort(citations.begin(), citations.end(), greater<int>());
+     for (int i = 0; i < citations.size(); i++) {
+        if (citations[i] >= i+1) {
+            answer = i+1;
+        }  
+    }
+    
+    return answer;
+}
